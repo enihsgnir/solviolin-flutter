478bd5a24cd7de1158b0c4a4b8215af795d5b89a
enihsgnir <enihsgnir@gmail.com>
2024-11-06 21:10:37 +0900

    reset project

--- solviolin-admin/android/app/src/main/kotlin/com/solviolin/solviolin_admin/MainActivity.kt
+++ solviolin-admin/android/app/src/main/kotlin/com/solviolin/solviolin_admin/MainActivity.kt
@@ -2,5 +2,4 @@ package com.solviolin.solviolin_admin
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- solviolin/android/app/src/main/kotlin/com/solviolinTeam/user/solviolin/MainActivity.kt
+++ solviolin/android/app/src/main/kotlin/com/solviolinTeam/user/solviolin/MainActivity.kt
@@ -2,5 +2,4 @@ package com.solviolinTeam.user.solviolin
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
